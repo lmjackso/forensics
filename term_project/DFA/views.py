cb2fe574210f7806ae833509965e9726917564b3
larryjiii@hotmail.com <lmjackso@Lawrences-MacBook-Pro.local>
2013-12-07 23:29:03 -0500

    added dependencies

--- term_project/DFA/views.py
+++ term_project/DFA/views.py
@@ -1,1 +1,25 @@
 # Create your views here.
+
+from hachoir_core.error import HachoirError
+from hachoir_core.stream import InputIOStream
+from hachoir_parser import guessParser
+from hachoir_metadata import extractMetadata
+
+
+def metadata_for_filelike(filelike):
+      try:
+        filelike.seek(0)
+      except (AttributeError, IOError):
+        return None
+
+      stream = InputIOStream(filelike, None, tags=[])
+      parser = guessParser(stream)
+
+      if not parser:
+        return None
+      try:
+        metadata = extractMetadata(parser)
+      except HachoirError:
+        return None
+
+    return metadata
